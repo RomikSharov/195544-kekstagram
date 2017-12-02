69d2f02134f53134ae90eaddb046d6e32f9417ef
RomikSharov <semen.ok2@gmail.com>
2017-12-02 16:07:26 +0400

    Сделал, попытка №1

--- js/pictures.js
+++ js/pictures.js
@@ -23,6 +23,21 @@ var popupLikes = popup.querySelector('.likes-count');
 var popupComments = popup.querySelector('.comments-count');
 var popupClose = popup.querySelector('.gallery-overlay-close');
 
+//  ******************* module4-task2*****************
+var uploadPopup = document.querySelector('.upload-overlay');
+var uploadFile = document.querySelector('#upload-file');
+var uploadComment = document.querySelector('.upload-form-description');
+var uploadSubmit = document.querySelector('#upload-submit');
+
+var uploadResize = document.querySelector('.upload-resize-controls-value');
+var buttonDec = document.querySelector('.upload-resize-controls-button-dec');
+var buttonInc = document.querySelector('.upload-resize-controls-button-inc');
+var imagePreview = document.querySelector('.effect-image-preview');
+
+var radioContainer = document.querySelector('.upload-effect-controls');
+
+var hashtagsElem = uploadPopup.querySelector('.upload-form-hashtags');
+//  *******************************************
 function getPhotos() {
   var arr = [];
   for (var i = 1; i <= photoCount; i++) {
@@ -57,7 +72,9 @@ function renderPopup(pic) {
   popupComments.textContent = pic.comments.length;
 
   popup.classList.remove('hidden');
-  popupClose.setAttribute('tabindex', 0);
+  if (!popupClose.getAttribute('tabindex')) {
+    popupClose.setAttribute('tabindex', 0);
+  }
 }
 
 //  ************************************************
@@ -71,13 +88,80 @@ function openPopup(evt, data) {
 }
 function closePopup(evt) {
   if (evt.target.closest('.gallery-overlay-close') || event.keyCode === ESC_KEYCODE) {
-    popup.classList.add('hidden');
-
-    popupClose.removeAttribute('tabindex');
+    if (!popup.classList.contains('hidden')) {
+      popup.classList.add('hidden');
+    }
   }
 }
-
 //  ************************************************
+//  ******************* module4-task2*****************
+function openUploadFile(evt) {
+  evt.preventDefault();
+  uploadPopup.classList.remove('hidden');
+}
+function closeUploadFile(evt) {
+  if (evt.target.closest('.upload-form-cancel') || (event.keyCode === ESC_KEYCODE && evt.target !== uploadComment)) {
+    if (!uploadPopup.classList.contains('hidden')) {
+      uploadPopup.classList.add('hidden');
+    }
+  }
+}
+function resizeFile(evt) {
+  var curValue = parseInt(uploadResize.getAttribute('value'), 10);
+
+  if (evt.target === buttonDec) {
+    curValue = Math.max(25, curValue - 25);
+  } else {
+    curValue = Math.min(100, curValue + 25);
+  }
+  uploadResize.setAttribute('value', curValue);
+  imagePreview.style.transform = 'scale(' + curValue / 100 + ')';
+}
+function applyEffect(evt) {
+  var elt = evt.target.closest('input[type="radio"]');
+  if (elt) {
+    if (imagePreview.classList.length === 2) {
+      imagePreview.classList.remove(imagePreview.classList[1]);
+    }
+    imagePreview.classList.add(elt.id.substring('upload-'.length));
+  }
+}
+function checkHashtags() {
+  if (hashtagsElem.value.length === 0) {
+    return false;
+  }
+  var hashtags = hashtagsElem.value.split(' ');
+  var length = hashtags.length;
+  //  проверяю количество хэш-тегов
+  if (length > 5) {
+    return true;
+  }
+
+  for (var i = 0; i < length; i++) {
+    //  проверяю первый символ хэш-тега
+    if (hashtags[i][0] !== '#') {
+      return true;
+    }
+    //  проверяю длину хэш-тега
+    if (hashtags[0].length > 20) {
+      return true;
+    }
+    //  проверяю совпадение хэш-тегов
+    for (var j = 0; j < length; j++) {
+      if (hashtags[i].toLowerCase() === hashtags[j].toLowerCase() && i !== j) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+function checkForm(evt) {
+  if (checkHashtags()) {
+    hashtagsElem.style.borderColor = 'red';
+    evt.preventDefault();
+  }
+}
+//  *************************************************
 
 var photoArr = getPhotos();
 
@@ -90,4 +174,20 @@ document.addEventListener('keydown', keyDownHendler(closePopup, ESC_KEYCODE));
 document.addEventListener('keydown', keyDownHendler(openPopup, ENTER_KEYCODE, photoArr));
 document.addEventListener('keydown', keyDownHendler(closePopup, ENTER_KEYCODE));
 
+//  ******************* module4-task2*****************
+uploadFile.addEventListener('change', openUploadFile);
+uploadPopup.addEventListener('click', closeUploadFile);
+document.addEventListener('keydown', keyDownHendler(closeUploadFile, ESC_KEYCODE));
+document.addEventListener('keydown', keyDownHendler(closeUploadFile, ENTER_KEYCODE));
+
+document.querySelector('#upload-select-image').setAttribute('action', 'https://js.dump.academy/kekstagram');
+document.querySelector('.upload-form-description').setAttribute('maxlength', '140');
+
+uploadResize.setAttribute('value', '100');
+buttonDec.addEventListener('click', resizeFile);
+buttonInc.addEventListener('click', resizeFile);
+
+radioContainer.addEventListener('click', applyEffect);
+
+uploadSubmit.addEventListener('click', checkForm);
 
