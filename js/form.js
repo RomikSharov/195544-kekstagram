48b8ba2f887252c7db1cbb98f035dc26dc8fffd4
RomikSharov <semen.ok2@gmail.com>
2017-12-03 22:50:13 +0400

    module5-task2 part1

--- js/form.js
+++ js/form.js
@@ -14,6 +14,81 @@
 
   var hashtagsElem = uploadPopup.querySelector('.upload-form-hashtags');
 
+  //  ***************************** module5-task2
+  var slider = uploadPopup.querySelector('.upload-effect-level-pin');
+  var bar = uploadPopup.querySelector('.upload-effect-level-val');
+  var sliderUploadValue = uploadPopup.querySelector('.upload-effect-level-value');
+
+  var sliderParent = slider.parentElement;
+  slider.setAttribute('draggable', true);
+  var startCoords = {
+    offsetLeft: 0,
+    offsetHeight: 0,
+    clientX: 0,
+    clientY: 0
+  };
+  slider.addEventListener('dragstart', onDragstartSlider);
+  slider.addEventListener('drag', onDragSlider);
+  slider.addEventListener('dragend', onDropSlider);
+  function onDragstartSlider(evt) {
+    //  evt.preventDefault();
+    evt.dataTransfer.setData('text/plain', evt.target.alt);
+    //  evt.dataTransfer.effectAllowed = 'move';
+    //  slider.style['background'] = 'red';
+    startCoords.offsetLeft = slider.offsetLeft;
+    startCoords.offsetHeight = slider.offsetHeight;
+    startCoords.clientX = evt.clientX;
+    startCoords.clientY = evt.clientY;
+  }
+  function onDragSlider(evt) {
+    evt.preventDefault();
+    var shift = {
+      x: evt.clientX - startCoords.clientX,
+      y: evt.clientY - startCoords.clientY
+    };
+    var newValue = 0;
+
+    if (shift.x > 364) {
+      newValue = 100;
+    } else if (shift.x < -91) {
+      newValue = 0;
+    } else {
+      newValue = (startCoords.offsetLeft + shift.x) / (455 / 100);
+    }
+
+    slider.style.left = newValue + '%';
+    bar.style['width'] = newValue + '%';
+    sliderUploadValue.setAttribute('value', Math.round(newValue));
+
+    var eff = 0;
+    if (imagePreview.classList.length === 2) {
+      eff = imagePreview.classList[1];
+      if (eff === 'effect-chrome') {
+        imagePreview.style['filter'] = 'grayscale(' + 0.01 * newValue + ')';
+      } if (eff === 'effect-sepia') {
+        imagePreview.style['filter'] = 'sepia(' + 0.01 * newValue + ')';
+      } if (eff === 'effect-marvin') {
+        imagePreview.style['filter'] = 'invert(' + newValue + '%)';
+      } if (eff === 'effect-phobos') {
+        imagePreview.style['filter'] = 'blur(' + 0.03 * newValue + 'px)';
+      } if (eff === 'effect-heat') {
+        imagePreview.style['filter'] = 'brightness(' + 0.03 * newValue + ')';
+      }
+    }
+  }
+
+  function onDropSlider(evt) {
+    evt.preventDefault();
+  }
+
+  //  ******************************************
+  // function initFilters() {
+  //   var filter = {
+  //     'effect-sepia' : {
+  //       defaultValue: uploadPopup.querySelector('.upload-effect-sepia').style[]
+  //     }
+  //   }
+  // }
   function resetDefault() {
     uploadResize.setAttribute('value', '100');
     imagePreview.style.transform = 'scale(1)';
