ac55e903a2f371c3706d15fe01ca826ad6fc2564
RomikSharov <semen.ok2@gmail.com>
2017-12-13 13:52:00 +0400

    Сортировка и задержка

--- js/data.js
+++ js/data.js
@@ -1,15 +1,22 @@
 'use strict';
 (function () {
+  var photoData = [];
 
   function onLoad(photosData) {
+    photoData = photosData;
     window.gallery.renderPictures(photosData);
     window.gallery.registerEventHandlers(photosData);
+
+    window.sort.initilize();
   }
 
   window.data = {
 
     getPhotos: function () {
       window.backend.load(onLoad, window.utils.onError);
-    }
+    },
+    getPhotoData: function () {
+      return photoData;
+    },
   };
 })();
